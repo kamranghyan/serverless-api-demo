c992c36a6fac84e2c7582a327ea70225ce1e4b2a
kamranghyan <kamranghyan@gmail.com>
2023-12-05 12:02:55 +0500

    Step 4: Setup Sample API using Serverless Offline

--- serverless-api/handler.js
+++ serverless-api/handler.js
@@ -1,18 +1,17 @@
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.sender = async (event) => {
+  try {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Hello from serverless! Message sent to SQS.' }),
+    };
+    return response;
+  } catch (error) {
+    console.error('Error sending message to SQS:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal server error.' }),
+    };
+  }
 };
